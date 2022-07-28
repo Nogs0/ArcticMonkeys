a8f11945aca120a862a4713a26836b61e04b592a
João Nogueira <joaoguinogueira04@gmail.com>
2022-07-28 16:02:14 -0300

    Teste Arctic Monkeys

---
+++ JS/meuscript.js
@@ -0,0 +1,37 @@
+
+function albuns(){
+    const discos = document.getElementById('discos');
+    discos.classList.toggle('active')
+}
+function albuns1(){
+    const am = document.getElementById('am');
+    const t1 = document.getElementById('t1');
+    am.classList.toggle('active');
+    t1.classList.toggle('active');
+    
+}
+function albuns2(){
+    const favourite = document.getElementById('favourite');
+    const t2 = document.getElementById('t2');
+    favourite.classList.toggle('active');
+    t2.classList.toggle('active');
+}
+function albuns3(){
+    const whatever = document.getElementById('whatever');
+    const t3 = document.getElementById('t3');
+    whatever.classList.toggle('active');
+    t3.classList.toggle('active');
+}
+function albuns4(){
+    const humbug = document.getElementById('humbug');
+    const t4 = document.getElementById('t4');
+    humbug.classList.toggle('active');
+    t4.classList.toggle('active');
+
+}
+function albuns5(){
+    const tranquility = document.getElementById('tranquility');
+    const t5 = document.getElementById('t5');
+    tranquility.classList.toggle('active');
+    t5.classList.toggle('active');
+}
